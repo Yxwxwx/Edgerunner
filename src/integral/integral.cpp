25f6fd16525c9c4b4a20ea685443b40a9efabe35
Sun Xinyu <sunxinyu347@gmail.com>
2024-12-03 15:24:43 +0800

    modified for MPI

--- src/integral/integral.cpp
+++ src/integral/integral.cpp
@@ -68,43 +68,53 @@ auto Integral::calc_int2e() -> void
     CINTOpt* opt = NULL;
     cint2e_sph_optimizer(&opt, _atm.data(), _natm, _bas.data(), _nbas, _env.data());
 
-#pragma omp parallel for schedule(dynamic) shared(opt)
+    std::vector<std::tuple<int, int, int, int>> ijkl;
 
     for (int i = 0; i < _nbas; i++) {
         for (int j = i; j < _nbas; j++) {
             for (int k = 0; k < _nbas; k++) {
                 for (int l = k; l < _nbas; l++) {
                     if (std::tuple(i, j) <= std::tuple(k, l)) {
-                        int shls[] = { i, j, k, l };
-
-                        int di = CINTcgto_spheric(i, _bas.data());
-                        int x = CINTtot_cgto_spheric(_bas.data(), i);
-
-                        int dj = CINTcgto_spheric(j, _bas.data());
-                        int y = CINTtot_cgto_spheric(_bas.data(), j);
-
-                        int dk = CINTcgto_spheric(k, _bas.data());
-                        int z = CINTtot_cgto_spheric(_bas.data(), k);
-
-                        int dl = CINTcgto_spheric(l, _bas.data());
-                        int h = CINTtot_cgto_spheric(_bas.data(), l);
-
-                        Eigen::Tensor<double, 4> buf_i(di, dj, dk, dl);
-                        cint2e_sph(buf_i.data(), shls, _atm.data(), _natm, _bas.data(), _nbas, _env.data(), opt);
-#pragma omp critical
-                        {
-                            _I.slice(Eigen::array<int, 4> { x, y, z, h }, Eigen::array<int, 4> { di, dj, dk, dl }) = buf_i;
-                            _I.slice(Eigen::array<int, 4> { y, x, z, h }, Eigen::array<int, 4> { dj, di, dk, dl }) = buf_i.shuffle(Eigen::array<int, 4> { 1, 0, 2, 3 });
-                            _I.slice(Eigen::array<int, 4> { x, y, h, z }, Eigen::array<int, 4> { di, dj, dl, dk }) = buf_i.shuffle(Eigen::array<int, 4> { 0, 1, 3, 2 });
-                            _I.slice(Eigen::array<int, 4> { y, x, h, z }, Eigen::array<int, 4> { dj, di, dl, dk }) = buf_i.shuffle(Eigen::array<int, 4> { 1, 0, 3, 2 });
-                            _I.slice(Eigen::array<int, 4> { z, h, x, y }, Eigen::array<int, 4> { dk, dl, di, dj }) = buf_i.shuffle(Eigen::array<int, 4> { 2, 3, 0, 1 });
-                            _I.slice(Eigen::array<int, 4> { h, z, x, y }, Eigen::array<int, 4> { dl, dk, di, dj }) = buf_i.shuffle(Eigen::array<int, 4> { 3, 2, 0, 1 });
-                            _I.slice(Eigen::array<int, 4> { z, h, y, x }, Eigen::array<int, 4> { dk, dl, dj, di }) = buf_i.shuffle(Eigen::array<int, 4> { 2, 3, 1, 0 });
-                            _I.slice(Eigen::array<int, 4> { h, z, y, x }, Eigen::array<int, 4> { dl, dk, dj, di }) = buf_i.shuffle(Eigen::array<int, 4> { 3, 2, 1, 0 });
-                        }
+                        ijkl.emplace_back(i, j, k, l);
                     }
                 }
             }
+        }
+    }
+
+    int tot = ijkl.size();
+
+#pragma omp parallel for shared(ijkl, tot, opt)
+    for (auto t = 0; t < tot; t++) {
+
+        auto [i, j, k, l] = ijkl[t];
+        int shls[] = { i, j, k, l };
+
+        int di = CINTcgto_spheric(i, _bas.data());
+        int x = CINTtot_cgto_spheric(_bas.data(), i);
+
+        int dj = CINTcgto_spheric(j, _bas.data());
+        int y = CINTtot_cgto_spheric(_bas.data(), j);
+
+        int dk = CINTcgto_spheric(k, _bas.data());
+        int z = CINTtot_cgto_spheric(_bas.data(), k);
+
+        int dl = CINTcgto_spheric(l, _bas.data());
+        int h = CINTtot_cgto_spheric(_bas.data(), l);
+
+        Eigen::Tensor<double, 4> buf_i(di, dj, dk, dl);
+        cint2e_sph(buf_i.data(), shls, _atm.data(), _natm, _bas.data(), _nbas, _env.data(), opt);
+
+#pragma omp critical
+        {
+            _I.slice(Eigen::array<int, 4> { x, y, z, h }, Eigen::array<int, 4> { di, dj, dk, dl }) = buf_i;
+            _I.slice(Eigen::array<int, 4> { y, x, z, h }, Eigen::array<int, 4> { dj, di, dk, dl }) = buf_i.shuffle(Eigen::array<int, 4> { 1, 0, 2, 3 });
+            _I.slice(Eigen::array<int, 4> { x, y, h, z }, Eigen::array<int, 4> { di, dj, dl, dk }) = buf_i.shuffle(Eigen::array<int, 4> { 0, 1, 3, 2 });
+            _I.slice(Eigen::array<int, 4> { y, x, h, z }, Eigen::array<int, 4> { dj, di, dl, dk }) = buf_i.shuffle(Eigen::array<int, 4> { 1, 0, 3, 2 });
+            _I.slice(Eigen::array<int, 4> { z, h, x, y }, Eigen::array<int, 4> { dk, dl, di, dj }) = buf_i.shuffle(Eigen::array<int, 4> { 2, 3, 0, 1 });
+            _I.slice(Eigen::array<int, 4> { h, z, x, y }, Eigen::array<int, 4> { dl, dk, di, dj }) = buf_i.shuffle(Eigen::array<int, 4> { 3, 2, 0, 1 });
+            _I.slice(Eigen::array<int, 4> { z, h, y, x }, Eigen::array<int, 4> { dk, dl, dj, di }) = buf_i.shuffle(Eigen::array<int, 4> { 2, 3, 1, 0 });
+            _I.slice(Eigen::array<int, 4> { h, z, y, x }, Eigen::array<int, 4> { dl, dk, dj, di }) = buf_i.shuffle(Eigen::array<int, 4> { 3, 2, 1, 0 });
         }
     }
     CINTdel_optimizer(&opt);
